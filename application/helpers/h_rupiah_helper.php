17a32b83cc1c71dce0944e1b57dd52fb9449cae1
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-02-29 09:26:14 +0700

    Remove Codeigniter Framework

--- application/helpers/h_rupiah_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-function format_rupiah($amount){
-  $hasil_rupiah = "Rp " . number_format($amount,2,',','.');
-  return $hasil_rupiah;
-}
-
-?>
