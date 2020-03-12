d39e3554d4252b106acaa3bac27770215bd3bf62
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-03-12 16:24:55 +0700

    Add Bootstrap
    Matrix Admin Template

---
+++ app/Http/Controllers/Admin_C.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Admin_C extends Controller
+{
+
+    public function index()
+    {
+    
+    }
+
+
+    /*
+    * Main dashboard
+    */
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', 'Admin_C@dashboard');
