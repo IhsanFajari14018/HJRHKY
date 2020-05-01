1bc7458f6379a24aaf51cdd83035df75de0d4757
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-05-02 00:16:57 +0700

    redirect home in nav HJRHKY

--- resources/views/public/master_p.blade.php
+++ resources/views/public/master_p.blade.php
@@ -29,7 +29,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
-                <a class="navbar-brand" href="#">HJRHKY</a>
+                <a class="navbar-brand" href="/">HJRHKY</a>
                 <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
