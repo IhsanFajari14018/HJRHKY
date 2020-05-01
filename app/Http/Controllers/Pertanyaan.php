2a53bca51fba2bc32ff7094ae54cdec20b73a9ed
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-05-02 00:16:14 +0700

    Method to display answered question

--- app/Http/Controllers/Pertanyaan.php
+++ app/Http/Controllers/Pertanyaan.php
@@ -3,17 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\M_Pertanyaan;
 use Illuminate\Http\Request;
-use Session;
+use Illuminate\Support\Facades\DB;
 
+use Session;
+use App\Answers;
+use App\Questions;
 
 class Pertanyaan extends Controller
 {
 
     public function pertanyaan()
     {
-        $questioner = M_Pertanyaan::orderBy('id', 'desc')->get();
+        $questioner = Questions::orderBy('id', 'desc')->get();
+
+        // foreach ($questioner as $value) {
+        //     echo $value->id." - ";
+        //     echo $value->question."<br>";
+        //     if(!isset($value->answers->answer)){
+        //         echo "NULL";
+        //     }else{
+        //         echo $value->answers->answer;
+        //     }
+        //     echo "<br><br>";
+        // }
+
         return view('admin/pertanyaan', ['questioner' => $questioner]);
     }
 
@@ -24,7 +38,7 @@ class Pertanyaan extends Controller
 
     public function doTanya(Request $request)
     {
-        $questioner = new M_Pertanyaan;
+        $questioner = new Questions;
 
         $questioner->name = $request->name;
         $questioner->email = $request->email;
@@ -49,16 +63,29 @@ class Pertanyaan extends Controller
 
     public function jawab($id)
     {
-        // $questioner = M_Pertanyaan::find('id')->get();
-
-        return view('admin/jawab');
+        $question = Questions::find($id);
+        return view('admin/jawab', ['question' => $question]);
     }
 
-    public function doJawab()
+    public function jawaban()
     {
-        // $questioner = M_Pertanyaan::find('id')->get();
+        $question = Questions::with('answers')->has('answers')->get();
+        return view('admin/jawaban', ['question' => $question]);
+    }
 
-        // return 
+    public function doJawab(Request $request, $id)
+    {
+        $answer = new Answers();
+
+        $answer->answer = $request->editordata;
+        $answer->questions_id = $id;
+
+        $answer->save();
+
+        //set session
+        $this->setResponse('sukses');
+
+        return redirect('/dashboard/pertanyaan');
     }
 
     /**
@@ -66,7 +93,7 @@ class Pertanyaan extends Controller
      */
     public function hapus($id)
     {
-        $questioner = M_Pertanyaan::find($id);
+        $questioner = Questions::find($id);
         $questioner->delete();
 
         return redirect()->back();
