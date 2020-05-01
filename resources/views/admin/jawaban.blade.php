6739064d2839932931253691921a4ab01b9983d0
Ihsan Fajari <ihsan.fajari@gmail.com>
2020-05-02 00:15:55 +0700

    Create jawaban.blade.php

---
+++ resources/views/admin/jawaban.blade.php
@@ -0,0 +1,89 @@
+@extends('admin/master_a')
+
+@section('html_title')
+<title>Daftar Jawaban | Admin HJRHKY</title>
+@endsection
+
+@section('page_title')
+<h4 class="page-title">Daftar Jawaban</h4>
+@endsection
+
+@section('styling')
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/extra-libs/multicheck/multicheck.css') }}">
+<link href="{{ asset('/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css') }}" rel="stylesheet">
+<link href="{{ asset('/dist/css/style.min.css') }}" rel="stylesheet">
+<link href="{{ asset('/dist/css/style.css') }}" rel="stylesheet">
+@endsection
+
+@section('breadcrumb')
+<li class="breadcrumb-item active" aria-current="page">Daftar Jawaban</li>
+@endsection
+
+@section('main_content')
+<div class="row">
+    <div class="col-12">
+        <!-- DATATABLE -->
+        <div class="card">
+            <div class="card-body">
+                <!-- <h5 class="card-title">Basic Datatable</h5> -->
+                <div class="table-responsive">
+                    <table id="zero_config" class="table table-striped table-bordered">
+                        <thead>
+                            <tr class="text-center">
+                                <th>No</th>
+                                <th>Dari</th>
+                                <th>Dijawab Tanggal</th>
+                                <th>Pertanyaan</th>
+                                <th>Jawaban</th>
+                                <!-- <th>Status</th> -->
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($question as $q)
+                            <tr>
+                                <td>{{ $q->id }}</td>
+                                <td class="text-center">{{ $q->name }} <br> | <br> {{ $q->email }}</td>
+                                <td>{{ $q->answers->created_at }}</td>
+                                <td>{{ $q->question }}</td>
+                                <td><?php echo $q->answers->answer ?></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+@endsection
+
+@section('queries')
+<script src="{{ asset('/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="{{ asset('/assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('/assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="{{ asset('/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
+<script src="{{ asset('/assets/extra-libs/sparkline/sparkline.js') }}"></script>
+<!--Wave Effects -->
+<script src="{{ asset('/dist/js/waves.js') }}"></script>
+<!--Menu sidebar -->
+<script src="{{ asset('/dist/js/sidebarmenu.js') }}"></script>
+<!--Custom JavaScript -->
+<script src="{{ asset('/dist/js/custom.min.js') }}"></script>
+<!-- this page js -->
+<script src="{{ asset('/assets/extra-libs/multicheck/datatable-checkbox-init.js') }}"></script>
+<script src="{{ asset('/assets/extra-libs/multicheck/jquery.multicheck.js') }}"></script>
+<script src="{{ asset('/assets/extra-libs/DataTables/datatables.min.js') }}"></script>
+<script>
+    /*******************************************
+     * Basic Table - with disabled auto order  *
+     *******************************************/
+    $('#zero_config').DataTable({
+        "order": [
+            [0, "desc"]
+        ]
+    });
+</script>
+@endsection
