c1b3395e21c1e6812160c46d71c66bc70c9ef2a9
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-05-20 14:22:54 +0700

    insert hashtags + preview hashtags

--- app/Http/Controllers/Pertanyaan.php
+++ app/Http/Controllers/Pertanyaan.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 
 use Session;
 use App\Answers;
+use App\Hashtags;
 use App\Questions;
 
 class Pertanyaan extends Controller
@@ -44,12 +45,41 @@ class Pertanyaan extends Controller
         $questioner->email = $request->email;
         $questioner->question = $request->question;
 
-        $questioner->save();
+        $response = $questioner->save();
+        // print_r($response);
+
+        //adding hashtag:
+        $id = Questions::where('email', $questioner->email)
+            ->orderBy('id', 'desc')
+            ->first();
+
+        print_r($id);
+        $this->addHashtags($request->hashtag, $id->id);
 
         //set session
-        $this->setResponse('sukses');
+        if ($response) {
+            $this->setResponse('sukses');
+        } else {
+            $this->setResponse('error');
+        }
 
-        return redirect('/ask');
+        // return redirect('/ask');
+    }
+
+    private function addHashtags($Htags, $id)
+    {
+        print_r($Htags);
+        echo "<br>";
+        $splittedHtags = explode(" ", $Htags);
+        print_r($splittedHtags);
+
+        foreach ($splittedHtags as $key => $htag) {
+            $hashtags = new Hashtags; //must be reseted
+            $hashtags->tag = $htag;
+            $hashtags->questions_id = $id;
+            $hashtags->save();
+            echo "Hashtag inserted!";
+        }
     }
 
     private function setResponse($response)
@@ -88,10 +118,10 @@ class Pertanyaan extends Controller
         return redirect('/dashboard/pertanyaan');
     }
 
-    public function qa(){
+    public function qa()
+    {
         $question = Questions::with('answers')->has('answers')->get();
         return view('public/q-and-a', ['question' => $question]);
-
     }
 
     /**
