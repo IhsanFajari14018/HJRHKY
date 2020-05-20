88a71720d706e81d37861f7884c349997345a70e
IhsanFajari14018 <ihsan.fajari@gmail.com>
2020-05-20 14:22:23 +0700

    Create 2020_05_20_095623_create_hashtags_table.php

---
+++ database/migrations/2020_05_20_095623_create_hashtags_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHashtagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hashtags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tag');
+            $table->integer('questions_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hashtags');
+    }
+}
